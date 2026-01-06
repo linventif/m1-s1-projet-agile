fc3cd90e431c441338a4dceef6f0f9f7e2117c0f
Jbras29 <julienbras0@gmail.com>
2026-01-06 16:51:00 +0100

    ajout PrixJ et dispos Vehicule

--- projetpoo/src/main/java/fr/univ/m1/projetagile/core/domain/Vehicule.java
+++ projetpoo/src/main/java/fr/univ/m1/projetagile/core/domain/Vehicule.java
@@ -1,6 +1,9 @@
 package fr.univ.m1.projetagile.core.domain;
 
 import jakarta.persistence.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "vehicules")
@@ -23,9 +26,18 @@ public class Vehicule {
 
   private boolean disponible = true;
 
+  @Column(nullable = false)
+  private Double prixJ; // Prix journalier
+
+  @ElementCollection
+  @CollectionTable(name = "vehicule_periodes_dispo", joinColumns = @JoinColumn(name = "vehicule_id"))
+  private List<PeriodeDisponibilite> datesDisponibilite = new ArrayList<>();
+
   @ManyToOne
   @JoinColumn(name = "proprietaire_id")
   private Agent proprietaire;
+
+  
 
   // JPA exige un constructeur sans arguments
   protected Vehicule() {
@@ -73,5 +85,59 @@ public class Vehicule {
 
   public void setProprietaire(Agent proprietaire) {
     this.proprietaire = proprietaire;
+  }
+
+  public Double getPrixJ() {
+    return prixJ;
+  }
+
+  public void setPrixJ(Double prixJ) {
+    this.prixJ = prixJ;
+  }
+
+  public List<PeriodeDisponibilite> getDatesDisponibilite() {
+    return datesDisponibilite;
+  }
+
+  public void setDatesDisponibilite(List<PeriodeDisponibilite> datesDisponibilite) {
+    this.datesDisponibilite = datesDisponibilite;
+  }
+
+  public void ajouterPeriodeDisponibilite(LocalDate dateDebut, LocalDate dateFin) {
+    this.datesDisponibilite.add(new PeriodeDisponibilite(dateDebut, dateFin));
+  }
+
+  // Classe embarquée pour représenter une période de disponibilité
+  @Embeddable
+  public static class PeriodeDisponibilite {
+    @Column(name = "date_debut")
+    private LocalDate dateDebut;
+
+    @Column(name = "date_fin")
+    private LocalDate dateFin;
+
+    protected PeriodeDisponibilite() {
+    }
+
+    public PeriodeDisponibilite(LocalDate dateDebut, LocalDate dateFin) {
+      this.dateDebut = dateDebut;
+      this.dateFin = dateFin;
+    }
+
+    public LocalDate getDateDebut() {
+      return dateDebut;
+    }
+
+    public void setDateDebut(LocalDate dateDebut) {
+      this.dateDebut = dateDebut;
+    }
+
+    public LocalDate getDateFin() {
+      return dateFin;
+    }
+
+    public void setDateFin(LocalDate dateFin) {
+      this.dateFin = dateFin;
+    }
   }
 }
