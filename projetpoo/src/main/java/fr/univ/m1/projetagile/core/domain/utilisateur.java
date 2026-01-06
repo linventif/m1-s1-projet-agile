c50bdc9459e2b92f43ced0941f226fbadce83bd6
Jbras29 <julienbras0@gmail.com>
2026-01-06 15:37:00 +0100

    nom prenom géré dans AgentParticulier et Loueur

--- projetpoo/src/main/java/fr/univ/m1/projetagile/core/domain/utilisateur.java
+++ projetpoo/src/main/java/fr/univ/m1/projetagile/core/domain/utilisateur.java
@@ -11,12 +11,6 @@ public abstract class Utilisateur {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
-  @Column(nullable = false)
-  private String nom;
-
-  @Column(nullable = false)
-  private String prenom;
-
   @Column(nullable = false, unique = true)
   private String email;
 
@@ -27,31 +21,13 @@ public abstract class Utilisateur {
   protected Utilisateur() {
   }
 
-  public Utilisateur(String nom, String prenom, String email, String motDePasse) {
-    this.nom = nom;
-    this.prenom = prenom;
+  public Utilisateur(String email, String motDePasse) {
     this.email = email;
     this.motDePasse = motDePasse;
   }
 
   public Long getId() {
     return id;
-  }
-
-  public String getNom() {
-    return nom;
-  }
-
-  public void setNom(String nom) {
-    this.nom = nom;
-  }
-
-  public String getPrenom() {
-    return prenom;
-  }
-
-  public void setPrenom(String prenom) {
-    this.prenom = prenom;
   }
 
   public String getEmail() {
