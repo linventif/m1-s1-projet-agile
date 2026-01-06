e78313093a70218980553f399c2ecdeb3c993840
Jbras29 <julienbras0@gmail.com>
2026-01-06 15:23:41 +0100

    ajout classe Location

---
+++ projetpoo/src/main/java/fr/univ/m1/projetagile/core/domain/Location.java
@@ -0,0 +1,115 @@
+package fr.univ.m1.projetagile.core.domain;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "locations")
+public class Location {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false)
+  private LocalDateTime dateDebut;
+
+  @Column(nullable = false)
+  private LocalDateTime dateFin;
+
+  @Column(nullable = true)
+  private String lieuDepot;
+
+  @Enumerated(EnumType.STRING)
+  @Column(nullable = false)
+  private StatutLocation statut = StatutLocation.EN_ATTENTE;
+
+  @ManyToOne
+  @JoinColumn(name = "vehicule_id", nullable = false)
+  private Vehicule vehicule;
+
+  @ManyToOne
+  @JoinColumn(name = "loueur_id", nullable = false)
+  private Loueur loueur;
+
+  // JPA exige un constructeur sans arguments
+  protected Location() {
+  }
+
+  public Location(LocalDateTime dateDebut, LocalDateTime dateFin, Vehicule vehicule, Loueur loueur) {
+    this.dateDebut = dateDebut;
+    this.dateFin = dateFin;
+    this.vehicule = vehicule;
+    this.loueur = loueur;
+  }
+
+  public Location(LocalDateTime dateDebut, LocalDateTime dateFin, String lieuDepot, Vehicule vehicule, Loueur loueur) {
+    this.dateDebut = dateDebut;
+    this.dateFin = dateFin;
+    this.lieuDepot = lieuDepot;
+    this.vehicule = vehicule;
+    this.loueur = loueur;
+  }
+
+  // Getters et setters
+  public Long getId() {
+    return id;
+  }
+
+  public LocalDateTime getDateDebut() {
+    return dateDebut;
+  }
+
+  public void setDateDebut(LocalDateTime dateDebut) {
+    this.dateDebut = dateDebut;
+  }
+
+  public LocalDateTime getDateFin() {
+    return dateFin;
+  }
+
+  public void setDateFin(LocalDateTime dateFin) {
+    this.dateFin = dateFin;
+  }
+
+  public String getLieuDepot() {
+    return lieuDepot;
+  }
+
+  public void setLieuDepot(String lieuDepot) {
+    this.lieuDepot = lieuDepot;
+  }
+
+  public StatutLocation getStatut() {
+    return statut;
+  }
+
+  public void setStatut(StatutLocation statut) {
+    this.statut = statut;
+  }
+
+  public Vehicule getVehicule() {
+    return vehicule;
+  }
+
+  public void setVehicule(Vehicule vehicule) {
+    this.vehicule = vehicule;
+  }
+
+  public Loueur getLoueur() {
+    return loueur;
+  }
+
+  public void setLoueur(Loueur loueur) {
+    this.loueur = loueur;
+  }
+
+  // Énumération pour le statut
+  public enum StatutLocation {
+    EN_ATTENTE,
+    ACCEPTE,
+    EN_COURS,
+    TERMINE,
+    ANNULE
+  }
+}
