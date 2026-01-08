c09f61e4a85bf5b3c1d6ea5b10eba9f1a77d570a
linventif <gregoire.becue@pm.me>
2026-01-08 13:16:26 +0100

    refact: improve formatting and readability of package documentation

--- projetpoo/src/main/java/fr/univ/m1/projetagile/messagerie/package-info.java
+++ projetpoo/src/main/java/fr/univ/m1/projetagile/messagerie/package-info.java
@@ -1,11 +1,14 @@
 /**
  * Package de messagerie pour la plateforme de location de véhicules.
- * 
- * <p>Ce package fournit un système de messagerie complet permettant aux utilisateurs
- * (Agents et Loueurs) de communiquer entre eux. Il est conçu comme un module indépendant
- * qui s'appuie sur le core métier sans le modifier.</p>
- * 
+ *
+ * <p>
+ * Ce package fournit un système de messagerie complet permettant aux utilisateurs (Agents et
+ * Loueurs) de communiquer entre eux. Il est conçu comme un module indépendant qui s'appuie sur le
+ * core métier sans le modifier.
+ * </p>
+ *
  * <h2>Architecture</h2>
+ * 
  * <pre>
  * messagerie/
  * ├── entity/           (Entités JPA)
@@ -14,33 +17,34 @@
  * │   └── MessageRepository
  * └── Main              (Point d'entrée de démonstration)
  * </pre>
- * 
+ *
  * <h2>Fonctionnalités principales</h2>
  * <ul>
- *   <li>Envoi et réception de messages entre utilisateurs</li>
- *   <li>Récupération de l'historique des messages</li>
- *   <li>Gestion des conversations entre deux utilisateurs</li>
- *   <li>Validation du contenu des messages</li>
+ * <li>Envoi et réception de messages entre utilisateurs</li>
+ * <li>Récupération de l'historique des messages</li>
+ * <li>Gestion des conversations entre deux utilisateurs</li>
+ * <li>Validation du contenu des messages</li>
  * </ul>
- * 
+ *
  * <h2>Utilisation</h2>
+ * 
  * <pre>{@code
  * // Envoyer un message
  * MessageRepository repo = new MessageRepository();
  * Message msg = new Message("Bonjour!", loueur, agent);
  * repo.save(msg);
- * 
+ *
  * // Récupérer les messages d'un utilisateur
  * List<Message> messages = utilisateur.getMessages();
- * 
+ *
  * // Récupérer une conversation
  * List<Message> conversation = repo.findConversationBetween(user1, user2);
  * }</pre>
- * 
+ *
  * @see fr.univ.m1.projetagile.messagerie.entity.Message
  * @see fr.univ.m1.projetagile.messagerie.persistence.MessageRepository
  * @see fr.univ.m1.projetagile.core.entity.Utilisateur#getMessages()
- * 
+ *
  * @since 1.0
  * @version 1.0
  * @author Projet Agile M1
