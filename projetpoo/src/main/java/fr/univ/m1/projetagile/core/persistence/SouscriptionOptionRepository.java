da0997f639a048b3a0d4346d508f8db296ca9a49
souleymane2001diallo-ops <souleymane2001diallo@gmail.com>
2026-01-08 16:28:23 +0100

    Gesrtion des option payantes
    Permettre à l'agent de contracter une option
    Supprimer une option

---
+++ projetpoo/src/main/java/fr/univ/m1/projetagile/core/persistence/SouscriptionOptionRepository.java
@@ -0,0 +1,61 @@
+package fr.univ.m1.projetagile.core.persistence;
+
+import java.util.List;
+import fr.univ.m1.projetagile.core.entity.Location;
+import fr.univ.m1.projetagile.core.entity.Options;
+import fr.univ.m1.projetagile.core.entity.SouscriptionOption;
+import jakarta.persistence.EntityManager;
+
+public class SouscriptionOptionRepository {
+
+  private final EntityManager em;
+
+  public SouscriptionOptionRepository(EntityManager em) {
+    this.em = em;
+  }
+
+  /**
+   * Sauvegarde une souscription d’option (création ou mise à jour).
+   */
+  public SouscriptionOption save(SouscriptionOption souscription) {
+    if (souscription.getId() == null) {
+      em.persist(souscription);
+      return souscription;
+    } else {
+      return em.merge(souscription);
+    }
+  }
+
+  /**
+   * Recherche une option par son identifiant.
+   */
+  public Options findOptionById(Long optionId) {
+    return em.find(Options.class, optionId);
+  }
+
+  /**
+   * Recherche une location par son identifiant.
+   */
+  public Location findLocationById(Long locationId) {
+    return em.find(Location.class, locationId);
+  }
+
+  /**
+   * Liste toutes les souscriptions associées à une location.
+   */
+  public List<SouscriptionOption> findByLocation(Long locationId) {
+    return em.createQuery("SELECT s FROM SouscriptionOption s WHERE s.location.id = :id",
+        SouscriptionOption.class).setParameter("id", locationId).getResultList();
+  }
+
+  /**
+   * Supprime une souscription d’option.
+   */
+  public void delete(SouscriptionOption souscription) {
+    SouscriptionOption managed = souscription;
+    if (!em.contains(souscription)) {
+      managed = em.merge(souscription);
+    }
+    em.remove(managed);
+  }
+}
