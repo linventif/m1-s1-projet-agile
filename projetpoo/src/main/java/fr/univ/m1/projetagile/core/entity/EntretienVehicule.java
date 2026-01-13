7dd6b64330fa0f6115b5015b33a01b78ca31c43b
Jbras29 <julienbras0@gmail.com>
2026-01-13 14:47:38 +0100

    Update EntretienVehicule.java

--- projetpoo/src/main/java/fr/univ/m1/projetagile/core/entity/EntretienVehicule.java
+++ projetpoo/src/main/java/fr/univ/m1/projetagile/core/entity/EntretienVehicule.java
@@ -1,6 +1,5 @@
 package fr.univ.m1.projetagile.core.entity;
 
-import java.time.LocalDate;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -18,10 +17,6 @@ public class EntretienVehicule {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
-  /**
-   * true si l'entretien a été déclenché automatiquement (par la plateforme), false si décidé /
-   * programmé manuellement.
-   */
   @Column(nullable = false)
   private Boolean automatique;
 
@@ -33,40 +28,16 @@ public class EntretienVehicule {
   @JoinColumn(name = "entretien_id", nullable = false)
   private Entretien entretien;
 
-  /**
-   * Date à laquelle l'entretien a été effectué.
-   */
-  @Column(name = "date_entretien", nullable = false)
-  private LocalDate dateEntretien;
-
-  /**
-   * Kilométrage du véhicule au moment de cet entretien.
-   */
-  @Column(name = "kilometrage_vehicule", nullable = false)
-  private Integer kilometrageVehicule;
-
   // Constructeur sans argument pour JPA
   protected EntretienVehicule() {}
 
-  // Ancien constructeur (si ton code l'utilise encore, tu peux garder)
   public EntretienVehicule(Boolean automatique, Vehicule vehicule, Entretien entretien) {
     this.automatique = automatique;
     this.vehicule = vehicule;
     this.entretien = entretien;
   }
 
-  // Constructeur complet recommandé
-  public EntretienVehicule(Boolean automatique, Vehicule vehicule, Entretien entretien,
-      LocalDate dateEntretien, Integer kilometrageVehicule) {
-    this.automatique = automatique;
-    this.vehicule = vehicule;
-    this.entretien = entretien;
-    this.dateEntretien = dateEntretien;
-    this.kilometrageVehicule = kilometrageVehicule;
-  }
-
-  // Getters / Setters
-
+  // Getters et Setters
   public Long getId() {
     return id;
   }
@@ -93,21 +64,5 @@ public class EntretienVehicule {
 
   public void setEntretien(Entretien entretien) {
     this.entretien = entretien;
-  }
-
-  public LocalDate getDateEntretien() {
-    return dateEntretien;
-  }
-
-  public void setDateEntretien(LocalDate dateEntretien) {
-    this.dateEntretien = dateEntretien;
-  }
-
-  public Integer getKilometrageVehicule() {
-    return kilometrageVehicule;
-  }
-
-  public void setKilometrageVehicule(Integer kilometrageVehicule) {
-    this.kilometrageVehicule = kilometrageVehicule;
   }
 }
